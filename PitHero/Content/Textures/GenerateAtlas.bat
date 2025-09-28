e1b5c7e83e44c8b43d1d9599c84df05e7a392cf2
rpillai25 <rpillai25@gmail.com>
2025-09-28 17:17:00 -0500

    Add potion items to atlas

--- PitHero/Content/Textures/GenerateAtlas.bat
+++ PitHero/Content/Textures/GenerateAtlas.bat
@@ -1,2 +1,3 @@
 mono C:\Users\Robin\Development\Nez\Nez.SpriteAtlasPacker\PrebuiltExecutable\SpriteAtlasPacker.exe -image:UI.png -map:UI.atlas -fps:15 UI -pow2
 mono C:\Users\Robin\Development\Nez\Nez.SpriteAtlasPacker\PrebuiltExecutable\SpriteAtlasPacker.exe -image:Actors.png -map:Actors.atlas -fps:8 Actors -pow2
+mono C:\Users\Robin\Development\Nez\Nez.SpriteAtlasPacker\PrebuiltExecutable\SpriteAtlasPacker.exe -image:Items.png -map:Items.atlas -fps:8 Items -pow2
