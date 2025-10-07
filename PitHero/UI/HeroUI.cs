06499efcb648029d3a804f9bd7cebc5fe3942001
rpillai25 <rpillai25@gmail.com>
2025-10-07 06:22:38 -0500

    Sort order arrow works correctly

--- PitHero/UI/HeroUI.cs
+++ PitHero/UI/HeroUI.cs
@@ -53,6 +53,10 @@ namespace PitHero.UI
         private Image _sortTimeArrow;
         private Image _sortTypeArrow;
         private Image _sortNameArrow;
+        // Arrow drawables to control flipping
+        private SpriteDrawable _sortTimeArrowDrawable;
+        private SpriteDrawable _sortTypeArrowDrawable;
+        private SpriteDrawable _sortNameArrowDrawable;
         
         // Sort button styles (normal and pressed)
         private ImageButtonStyle _sortTimeNormalStyle;
@@ -233,13 +237,13 @@ namespace PitHero.UI
             _sortNameButton.SetSize(sortNameNormal.SourceRect.Width, sortNameNormal.SourceRect.Height);
             _sortNameButton.OnClicked += (btn) => OnSortButtonClicked(InventorySortOrder.Name);
             
-            // Create arrow images and keep drawables to control FlipY
-            var sortArrowDrawableTime = new SpriteDrawable(sortArrowSprite);
-            var sortArrowDrawableType = new SpriteDrawable(sortArrowSprite);
-            var sortArrowDrawableName = new SpriteDrawable(sortArrowSprite);
-            _sortTimeArrow = new Image(sortArrowDrawableTime);
-            _sortTypeArrow = new Image(sortArrowDrawableType);
-            _sortNameArrow = new Image(sortArrowDrawableName);
+            // Create arrow images using stored drawables so we can flip via SpriteEffects
+            _sortTimeArrowDrawable = new SpriteDrawable(sortArrowSprite);
+            _sortTypeArrowDrawable = new SpriteDrawable(sortArrowSprite);
+            _sortNameArrowDrawable = new SpriteDrawable(sortArrowSprite);
+            _sortTimeArrow = new Image(_sortTimeArrowDrawable);
+            _sortTypeArrow = new Image(_sortTypeArrowDrawable);
+            _sortNameArrow = new Image(_sortNameArrowDrawable);
             // Ensure arrows do not intercept input
             _sortTimeArrow.SetTouchable(Touchable.Disabled);
             _sortTypeArrow.SetTouchable(Touchable.Disabled);
@@ -340,24 +344,33 @@ namespace PitHero.UI
             
             // Show arrow on active button and flip vertically based on direction
             Image activeArrow = null;
+            SpriteDrawable activeDrawable = null;
             switch (currentSort)
             {
                 case InventorySortOrder.Time:
                     activeArrow = _sortTimeArrow;
+                    activeDrawable = _sortTimeArrowDrawable;
                     break;
                 case InventorySortOrder.Type:
                     activeArrow = _sortTypeArrow;
+                    activeDrawable = _sortTypeArrowDrawable;
                     break;
                 case InventorySortOrder.Name:
                     activeArrow = _sortNameArrow;
+                    activeDrawable = _sortNameArrowDrawable;
                     break;
             }
             
             if (activeArrow != null)
             {
                 bool ascending = currentDirection == SortDirection.Ascending;
-                // Flip by scaling Y to -1 for ascending (if API supports), fallback rotate if not
-                activeArrow.SetScaleY(ascending ? -1f : 1f);
+                // Flip vertically using SpriteEffects so it always renders up/down properly
+                if (activeDrawable != null)
+                {
+                    activeDrawable.SpriteEffects = ascending 
+                        ? Microsoft.Xna.Framework.Graphics.SpriteEffects.FlipVertically 
+                        : Microsoft.Xna.Framework.Graphics.SpriteEffects.None;
+                }
                 activeArrow.SetVisible(true);
             }
         }
