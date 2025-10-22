3b1095539f61ef03b693d5ac2307f6b10d200f4c
rpillai25 <rpillai25@gmail.com>
2025-10-21 23:54:58 -0500

    Fix PlaceholderMonsterAnimationComponent to actually show placeholder

--- PitHero/ECS/Components/PlaceholderMonsterAnimationComponent.cs
+++ PitHero/ECS/Components/PlaceholderMonsterAnimationComponent.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Nez;
 
 namespace PitHero.ECS.Components
 {
@@ -18,5 +19,48 @@ namespace PitHero.ECS.Components
         protected override string AnimLeft => "PlaceholderMonster";
         protected override string AnimRight => "PlaceholderMonster";
         protected override string AnimUp => "PlaceholderMonster";
+
+        public override void OnAddedToEntity()
+        {
+            // Don't call base, as it tries to load animations for animated enemies
+            try
+            {
+                var actorsAtlas = Core.Content.LoadSpriteAtlas("Content/Atlases/Actors.atlas");
+                if (actorsAtlas != null)
+                {
+                    var sprite = actorsAtlas.GetSprite("PlaceholderMonster");
+                    if (sprite != null)
+                    {
+                        this.Sprite = sprite;
+                        this.SetColor(ComponentColor);
+                        Debug.Log("[PlaceholderMonsterAnimationComponent] Loaded static sprite PlaceholderMonster");
+                    }
+                    else
+                    {
+                        Debug.Warn("[PlaceholderMonsterAnimationComponent] Sprite 'PlaceholderMonster' not found in Actors.atlas");
+                    }
+                }
+                else
+                {
+                    Debug.Warn("[PlaceholderMonsterAnimationComponent] Failed to load Actors.atlas - atlas is null");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Warn($"[PlaceholderMonsterAnimationComponent] Failed to load sprite: {ex.Message}");
+            }
+
+            // Add facing component if not present (for consistency with other enemies)
+            var facing = Entity.GetComponent<ActorFacingComponent>();
+            if (facing == null)
+            {
+                Entity.AddComponent(new ActorFacingComponent());
+            }
+        }
+
+        public new void Update()
+        {
+            // Do nothing - static sprite, no animation updates needed
+        }
     }
 }
