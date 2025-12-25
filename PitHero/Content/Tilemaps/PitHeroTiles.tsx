cb56b4e5f016288419d825fb2f680aa05c3eadeb
Robin Pillai <37321547+rpillai25@users.noreply.github.com>
2025-12-25 00:01:29 -0600

    Update map & tiles

--- PitHero/Content/Tilemaps/PitHeroTiles.tsx
+++ PitHero/Content/Tilemaps/PitHeroTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="PitHeroTiles" tilewidth="32" tileheight="32" tilecount="63" columns="9">
- <image source="PitHeroTiles.png" width="288" height="224"/>
+<tileset version="1.10" tiledversion="1.11.2" name="PitHeroTiles" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="PitHeroTiles.png" width="288" height="288"/>
 </tileset>
