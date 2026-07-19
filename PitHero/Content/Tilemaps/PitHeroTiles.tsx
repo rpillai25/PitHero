f20b375d17cf19be9875bdf9b6a66e2248db2ea8
Robin Pillai <37321547+rpillai25@users.noreply.github.com>
2026-07-19 16:43:58 -0500

    Add fridge tiles

--- PitHero/Content/Tilemaps/PitHeroTiles.tsx
+++ PitHero/Content/Tilemaps/PitHeroTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="PitHeroTiles" tilewidth="32" tileheight="32" tilecount="171" columns="9">
- <image source="PitHeroTiles.png" width="288" height="608"/>
+<tileset version="1.10" tiledversion="1.11.2" name="PitHeroTiles" tilewidth="32" tileheight="32" tilecount="180" columns="9">
+ <image source="PitHeroTiles.png" width="288" height="640"/>
 </tileset>
